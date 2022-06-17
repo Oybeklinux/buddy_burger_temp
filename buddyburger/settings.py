90cf24a58012f08b1b595cd3b71d4d10aca66f74
Oybeklinux <Oybeklinux@gmail.com>
2022-06-17 17:38:25 +0500

    static and db added to gitignore

--- buddyburger/settings.py
+++ buddyburger/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = env("STATIC_ROOT")#'/home/kannasu1/static/'#'/static/' #os.path.join(BASE_DIR, 'static')  # deploy uchun
+STATIC_ROOT = env("STATIC_ROOT") # deploy uchun
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),  # debug uchun assets
 ]
