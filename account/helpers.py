7e3beaff32a3ce4e94ba3a604f670fb7bc87c04b
Oybeklinux <Oybeklinux@gmail.com>
2022-06-17 17:08:13 +0500

    sms token env

--- account/helpers.py
+++ account/helpers.py
@@ -1,6 +1,9 @@
 import random
 import requests
 from django.conf import settings
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 
 def send_otp_to_phone(phone_number):
@@ -8,7 +11,7 @@ def send_otp_to_phone(phone_number):
         otp = random.randint(100000, 999999)
         url = 'https://api.smsfly.uz/'
         credentials = {
-            "key": "d6400af4-fa5f-11eb-9bd9-0242ac120002",
+            "key": env("token"), #"8f527b2c-e8d7-11ec-a71e-0242ac120002",
             "phone": str(phone_number),
             "message": str(otp)
         }
--- buddyburger/settings.py
+++ buddyburger/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # deploy uchun
+STATIC_ROOT = '/static/' #os.path.join(BASE_DIR, 'static')  # deploy uchun
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),  # debug uchun assets
 ]
