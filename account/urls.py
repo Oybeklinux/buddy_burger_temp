2ad7a7016da2a750dc693dd92317597a704db91e
Oybeklinux <Oybeklinux@gmail.com>
2022-06-27 14:54:13 +0500

    extra send sms added for test

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('send_sms/', send_sms_to_login),
+    path('send/', send_sms_to_login),
     path('login/', login_user),
     path('logout/', logout_user),
     path('register/', register_user)
