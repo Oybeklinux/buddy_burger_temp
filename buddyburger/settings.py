12fba5262fb00f793b4c5636f05759b2f9425d57
oybek <oybeklinux@gmail.com>
2022-08-02 17:31:57 +0500

    allowed_host, debug added to env file

--- buddyburger/settings.py
+++ buddyburger/settings.py
@@ -29,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-#ALLOWED_HOSTS = ['api.buddyburger.kannas.uz', 'www.api.buddyburger.kannas.uz',"api.buddyburger.uz", "www.api.buddyburger.uz", "127.0.0.1"]
-ALLOWED_HOSTS=['*']
+ALLOWED_HOSTS = [env('ALLOWED_HOST1'),env('ALLOWED_HOST2'), "127.0.0.1"]
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
